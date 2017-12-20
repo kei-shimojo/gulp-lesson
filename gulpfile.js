91f80ff20d9a93aca7a310210fa455c5b82b576f
kei-shimojo <shimokei0421@gmail.com>
2017-12-20 18:10:01 +0900

    lesson2

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,11 @@ var sass = require('gulp-sass');
 gulp.task('sass', function() {
     return gulp.src(['./src/sass/**/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest('./dist/cdd/'));
+        .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass'], function() {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+
